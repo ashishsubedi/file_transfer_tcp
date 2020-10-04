dd0d54edacd179bad6fc8098bd9e6a719135de52
Ashish Subedi <ashishsubedi10@gmail.com>
2020-10-04 21:49:27 +0545

    Updated for 1to 1 file transfer model with custom handshaking method

--- client.py
+++ client.py
@@ -1,25 +1,92 @@
 import socket
 from time import time
+import sys
 
-TCP_IP = 'ftserverhost.herokuapp.com'
-TCP_PORT = 80
-BUFFER_SIZE = 1024
+FLAG_SEND = 1
+FLAG_RECV = 0
+
+flag = -1
+recvFlag = -1
+filename = ''
+
+TCP_IP = '0.0.0.0'
+TCP_PORT = 7200
+BUFFER_SIZE = 4096
+
+TCP_IP = input("Enter SERVER address: ")
+TCP_PORT = int(input("Enter server port: "))
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((TCP_IP,TCP_PORT))
 
-start = time()
+print("Connection Establisehd with Server")
+def establishSendRecvConn():
+    global flag,recvFlag,filename
+    while(flag+recvFlag != 1):
+        flag = int(input("Enter 0- Receive \t 1- Send \t Press any other key to exit"))
+       
 
-with open('received_file','wb') as f:
-    print("Downloading file")
-    while True:
-        data = s.recv(BUFFER_SIZE)
-        print(data)
-        if not data:
-            break
-        f.write(data)
+        #Establish flag handshake
+        s.send(bytes(str(flag),'utf-8'))
+        '''
+            Response must be opposite of flag
+            For eg, if client is sending file (flag=1), then server must send recieving flag(flag=0) which sums to 1
+        '''
+        print("Waiting for Server's response")
+        recvFlag = int(s.recv(4).decode('utf-8'))
 
-end = time()
-print(f'File downloaded in {end-start} ms')
-s.close()
-        
+    if(flag+recvFlag == 1):return True
+    else: return False
+
+
+if __name__ == "__main__":
+    try: 
+        retry = 1
+        while (retry == 1 or retry == '1'):
+            if not establishSendRecvConn(): sys.exit(1)
+
+            if(flag == FLAG_RECV):
+                filename = input("Enter full path of file to receive")
+            elif flag == FLAG_SEND:
+                filename = input("Enter full path of file to send")
+            else:
+                print("Exiting program")
+                sys.exit(1)
+
+            if flag == FLAG_RECV:
+                try:
+                    with open(filename,'wb') as f:
+                        print("Downloading file")
+                        while True:
+                            data = s.recv(BUFFER_SIZE)
+                            print(data)
+                            if not data:
+                                break
+                            f.write(data)
+                except:
+                    print("Some error occured")
+                    retry = input("Press 1 to send file again, press any other key to exit")
+            
+            elif flag == FLAG_SEND:
+                try:
+                    with open(filename,'rb') as f:
+                        print("Uploading file")
+                        while True:
+                            l = f.read(BUFFER_SIZE)
+                            # while the file contains data after read
+                            while(l):
+                                s.send(l)
+                                l=f.read(BUFFER_SIZE)
+                            if not l:
+                                break
+                except:
+                    print("Some error occured")
+                    retry = input("Press 1 to send file again, press any other key to exit")
+            
+            
+            retry = input("Press 1 to send file again, press any other key to exit")
+    except:
+        pass
+    finally:
+        s.close()
+            
--- server.py
+++ server.py
@@ -3,49 +3,94 @@ from threading import Thread
 from socketserver import ThreadingMixIn
 
 TCP_IP = '0.0.0.0'
-TCP_PORT = 80
-BUFFER_SIZE = 1024
+TCP_PORT = 7200
+BUFFER_SIZE = 4096
 
-class ClientThread(Thread):
-    def __init__(self,ip: str,port: int,sock: socket.socket):
-        Thread.__init__(self)
-        self.ip = ip
-        self.port =  port
-        self.sock = sock
-        print(f"New thread started for {ip}:{port} ")
 
-    def run(self):
-        filename = 'file.txt'
-        with open(filename,'rb') as f:
-            while True:
-                l = f.read(BUFFER_SIZE)
-                # while the file contains data after read
-                while(l):
-                    self.sock.send(l)
-                    l=f.read(BUFFER_SIZE)
-                if not l:
-                    self.sock.close()
-                    break
+FLAG_SEND = 1
+FLAG_RECV = 0
+
+flag = -1
+recvFlag = -1
+filename = ''
+
+def establishSendRecvConn(sock):
+    global flag,recvFlag,filename
+    while(flag+recvFlag != 1):
+        flag = int(input("Enter 0- Receive \t 1- Send \t Press any other key to exit"))
+        '''
+            Response must be opposite of flag
+            For eg, if client is sending file (flag=1), then server must send recieving flag(flag=0) which sums to 1
+        '''
+        print("Waiting for client's Response")
+        recvFlag = int(sock.recv(4).decode('utf-8'))
+
+        #Establish flag handshake
+        sock.send(bytes(str(flag),'utf-8'))
+
+    if(flag+recvFlag == 1):return True
+    else: return False
+
+
 
 def startServer():
+    
     tcpsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     tcpsock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     tcpsock.bind((TCP_IP,TCP_PORT))
 
-    threads = []
+    # while True:
+    tcpsock.listen(1)
+    print("Waiting for incoming connections")
+    (sock,(ip,port)) = tcpsock.accept()
+    print(f"[CONNECTED] Welcome, {ip}:{port} !")
+    try:
+        retry = 1
+        while(retry == 1 or retry == '1'):
+            if not establishSendRecvConn(sock): return False
 
-    while True:
-        tcpsock.listen(5)
-        print("Waiting for incoming connections")
-        (conn,(ip,port)) = tcpsock.accept()
-        print(f"[CONNECTED] Welcome, {ip}:{port} !")
-        newThread = ClientThread(ip,port,conn)
-        newThread.start()
-        threads.append(newThread)
+            if(flag == FLAG_RECV):
+                filename = input("Enter full path of file to receive")
+            elif flag == FLAG_SEND:
+                filename = input("Enter full path of file to send")
+            else:
+                print("Exiting program")
+                return False
 
-    for t in threads:
-        t.join()
-    tcpsock.close()
+            if flag == FLAG_RECV:
+                try:
+                    with open(filename,'wb') as f:
+                        print("Downloading file")
+                        while True:
+                            data = sock.recv(BUFFER_SIZE)
+                            print(data)
+                            if not data:
+                                break
+                            f.write(data)
+                except:
+                    print("Some error occured")
+                    retry = input("Press 1 to send file again, press any other key to exit")
+            
+
+            elif flag == FLAG_SEND:
+                try:
+                    with open(filename,'rb') as f:
+                        print("Uploading file")
+                        while True:
+                            l = f.read(BUFFER_SIZE)
+                            # while the file contains data after read
+                            while(l):
+                                sock.send(l)
+                                l=f.read(BUFFER_SIZE)
+                            if not l:
+                                break
+                except:
+                    print("Some error occured")
+                    retry = input("Press 1 to send file again, press any other key to exit")
+    except:
+        pass
+    finally:
+        tcpsock.close()
 
 if __name__ == "__main__":
     startServer()
